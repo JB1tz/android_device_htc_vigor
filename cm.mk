bcc365a108f2fc947247874680383d19d6f4bc61
Jonathan Bither <jonbither@gmail.com>
2012-08-23 13:10:23 -0400

    new boot animation is square so drop the height from the zip name

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor TARGET_DEVICE=vigor BUILD_FINGERPRINT="verizon_wwe/htc_vigor/vigor:4.0.3/IML74K/372320.12:user/release-keys" PRIVATE_BUILD_DESC="3.14.605.12 CL372320 release-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+TARGET_BOOTANIMATION_NAME := vertical-720
 
 # Release name
 PRODUCT_NAME := cm_vigor
